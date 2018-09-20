56946cc24f393493072a9b53a188b39be1c6ddeb
PRBrown <philipparhodes@hotmail.com>
2018-09-20 17:24:12 -0400

    Added missing ',' on line 5

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -2,7 +2,7 @@ angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
       return $http.get('http://localhost:8080/api/listings');
-    }
+    },
 	
 	create: function(listing) {
 	  return $http.post('http://localhost:8080/api/listings', listing);
