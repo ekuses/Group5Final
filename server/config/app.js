08fc48eeccdd871bc7b5085cbc4f81ee7e4b7af2
Silian Hu <sihu@ufl.edu>
2018-09-24 17:39:32 -0400

    tryna get heroku to work

--- server/config/app.js
+++ server/config/app.js
@@ -5,6 +5,6 @@ var config = require('./config'),
 module.exports.start = function() {
   var app = express.init();
   app.listen(config.port, function() {
-    console.log('App listening on port', config.port);
+    console.log('App listening on port', process.env.PORT || config.port);
   });
 };
