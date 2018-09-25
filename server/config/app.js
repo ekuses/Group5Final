b409e751c2ac81b5fbefe4bbe19b3b96b454d852
Silian Hu <sihu@ufl.edu>
2018-09-25 10:48:36 -0400

    BC4.2

--- server/config/app.js
+++ server/config/app.js
@@ -3,8 +3,13 @@ var config = require('./config'),
     express = require('./express');
 
 module.exports.start = function() {
-  var app = express.init();
-  app.listen(config.port, function() {
-    console.log('App listening on port',config.port);
+    var app = express.init();
+    let port = process.env.PORT;
+    if (port == null || port == '')
+    {
+        port = config.port;
+    }
+  app.listen(port, function() {
+    console.log('App listening on port',port);
   });
 };
