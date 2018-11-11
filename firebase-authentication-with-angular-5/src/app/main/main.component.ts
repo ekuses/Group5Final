99393a0c8287d7d80d9ef5a45b71a68d5e88f18f
Jason Touchton <touchtonjason@gmail.com>
2018-11-11 15:33:57 -0500

    "got maps working in ts"

--- firebase-authentication-with-angular-5/src/app/main/main.component.ts
+++ firebase-authentication-with-angular-5/src/app/main/main.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import { } from '@types/googlemaps';
+import { ViewChild } from '@angular/core';
 
 
 @Component({
@@ -9,20 +11,29 @@ import { Observable } from 'rxjs';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
 
 
 
   ngOnInit() {
+    var mapProp = {
+          center: new google.maps.LatLng(29.6436, -82.3749),
+          zoom: 14,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+        setMarkers(this.map);
   }
 
-    moviesDb: Observable<any[]>;
+  tableshow: boolean;
 
-      theaterDb: Observable<any[]>;
+  moviesDb: Observable<any[]>;
+  theaterDb: Observable<any[]>;
   constructor(db: AngularFirestore) {
-    tableshow: boolean;
-
     this.moviesDb = db.collection('movies').valueChanges();
     this.theaterDb = db.collection('theaters').valueChanges();
+
   }
     showTable(){
     if(this.tableshow)
@@ -30,4 +41,29 @@ export class MainComponent implements OnInit {
     else
         this.tableshow = true;
   }
+
+  var theaters:string[]
+  theaters = [
+    ['Regal Butler Town Center 14', 29.6272, -82.3772, 3],
+    ['Regal Celebration Pointe 10 & RPX', 29.6237, -82.3953, 2],
+    ['Regal Cinemas Royal Park 16', 29.6539, -82.3802, 1]
+  ];
+
+  function setMarkers(map) {
+
+    var image = {
+      url: 'images/popcorn.svg'
+    };
+    for (var i = 0; i < theaters.length; i++) {
+      var theater = theaters[i];
+      var marker = new google.maps.Marker({
+        position: {lat: theater[1], lng: theater[2]},
+        map: map,
+        // icon: image,
+        title: theater[0],
+        zIndex: theater[3]
+      });
+    }
+  }
+
 }
