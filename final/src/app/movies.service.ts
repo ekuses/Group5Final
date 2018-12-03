67d875f6fc28e16b20fa33326a1446f391f90c48
DanielW27 <32105320+DanielW27@users.noreply.github.com>
2018-12-03 13:33:58 -0500

    Cleaned and fixed up the service and subscribe calls

--- final/src/app/movies.service.ts
+++ final/src/app/movies.service.ts
@@ -4,97 +4,37 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable()
 export class MoviesService { 
 
-  newberryMovies;
-  newberryMovieTimes;
-  celebrationMovies;
-  celebrationMovieTimes;
-  butlerMovies;
-  butlerMovieTimes;
-  allMovies;
-  allMovieTimes;
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'X-API-Key': 'LPhMgVoyBrmxG8uePgxCo4FHwJ6CbLdy'
+    })
+  };
 
   constructor(private httpClient: HttpClient) {}
 
-  //GET NEWBERRY MOVIES ON REQUEST
-  getNewberryMovies() {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        'X-API-Key': 'LPhMgVoyBrmxG8uePgxCo4FHwJ6CbLdy'
-      })
-    };
-
-    this.httpClient.get<any>('https://api.internationalshowtimes.com/v4/movies/?cinema_id=42506', httpOptions)
-      .subscribe((data: any) => {
-          this.newberryMovies = data;
-    });
+  //Get all movies from a specific cinema
+  getTheaterMovies(cinema_id) {
+    return this.httpClient.get<any>('https://api.internationalshowtimes.com/v4/movies/?cinema_id='+cinema_id, this.httpOptions);
   }
 
-//GET CELEBRATION MOVIES ON REQUEST
-  getCelebrationMovies() {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        'X-API-Key': 'LPhMgVoyBrmxG8uePgxCo4FHwJ6CbLdy'
-      })
-    };
-
-    this.httpClient.get<any>('https://api.internationalshowtimes.com/v4/movies/?cinema_id=64196', httpOptions)
-      .subscribe((data: any) => {
-          this.celebrationMovies = data;
-    });
-  }
-//GET BUTLER MOVIES ON REQUEST
-  getButlerMovies() {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        'X-API-Key': 'LPhMgVoyBrmxG8uePgxCo4FHwJ6CbLdy'
-      })
-    };
-
-    this.httpClient.get<any>('https://api.internationalshowtimes.com/v4/movies/?cinema_id=42490', httpOptions)
-      .subscribe((data: any) => {
-          this.butlerMovies = data;
-    });
-  }
-
+  //Get all movies in GNV
   getAllMovies(){
+    return this.httpClient.get<any>('https://api.internationalshowtimes.com/v4/movies/?location=29.6436,-82.3749&distance=30', this.httpOptions);
+  }
+
+
+  getShowtimesFromTheater(cinema_id, movie_id) {
+
+    return this.httpClient.get<any>('https://api.internationalshowtimes.com/v4/showtimes/?cinema_id='+cinema_id+'&movie_id='+movie_id, this.httpOptions);
 
   }
 
-  getShowtimesFromTheater(movie_id) {
-    //   $.ajax({
-    //   url: "https://api.internationalshowtimes.com/v4/movies/?cinema_id=" + movie_id,
-    //   type: "GET",
-    //   headers: {
-    //       "X-API-Key": "LPhMgVoyBrmxG8uePgxCo4FHwJ6CbLdy",
-    //   },
-    // })
-    // .done(function(data, textStatus, jqXHR) {
-    //       console.log("HTTP Request Succeeded: " + jqXHR.status);
-    //       console.log(data);
-    // })
-    // .fail(function(jqXHR, textStatus, errorThrown) {
-    //       console.log("HTTP Request Failed");
-    // })
-  }
-
+  //GET ALL SHOWTIMES FOR ALL MOVIES IN GAINESVILLE
   getShowtimesFromLocation(movie_id) {
-    //   $.ajax({
-    //   url: "https://api.internationalshowtimes.com/v4/movies/?location=29.6436,-82.3749&distance=30&movie_id=" + movie_id,
-    //   type: "GET",
-    //   headers: {
-    //       "X-API-Key": "LPhMgVoyBrmxG8uePgxCo4FHwJ6CbLdy",
-    //   },
-    // })
-    // .done(function(data, textStatus, jqXHR) {
-    //       console.log("HTTP Request Succeeded: " + jqXHR.status);
-    //       console.log(data);
-    // })
-    // .fail(function(jqXHR, textStatus, errorThrown) {
-    //       console.log("HTTP Request Failed");
-    // })
+
+    return this.httpClient.get<any>('https://api.internationalshowtimes.com/v4/showtimes/?location=29.6436,-82.3749&distance=30&movie_id=' + movie_id, this.httpOptions);
+
   }
 
 
--- final/src/app/movies/movies.component.ts
+++ final/src/app/movies/movies.component.ts
@@ -18,6 +18,16 @@ export class MoviesComponent implements OnInit {
   //establish the service to fetch movies and times
   // moviesService = new MoviesService(this.httpClient);
 
+  //cinema id's
+  newberryID = '42506';
+  celebrationID = '64196';
+  butlerID = '42490';
+
+
+  newberry = false;
+  celebration = false;
+  butler = false;
+
   //these are all arrays that will be filled with either times or movies
   newberryMovies;
   newberryMovieTimes;
@@ -37,25 +47,29 @@ export class MoviesComponent implements OnInit {
   showCelebration = 'none';
 
   ngOnInit() {
-    this.moviesService.getNewberryMovies();
-    this.moviesService.getCelebrationMovies();
-    this.moviesService.getButlerMovies() ;
-    this.moviesService.getAllMovies();
-    // this.moviesService.getMovies(42506).subscribe(
-    //   (response) => this.newberryMovies = response
-    // );
-    // this.moviesService.getMovies(64196).subscribe(
-    //   (response) => this.newberryMovies = response
-    // );
-    // this.moviesService.getMovies(42490).subscribe(
-    //   (response) => this.newberryMovies = response
-    // );
-    // this.moviesService.getAllMovies().subscribe(
-    //   (response) => this.allMovies = response
-    // );
+    this.moviesService.getTheaterMovies(this.newberryID).subscribe(
+      (response) => this.newberryMovies = response
+    );
+    this.moviesService.getTheaterMovies(this.celebrationID).subscribe(
+      (response) => this.celebrationMovies = response
+    );
+    this.moviesService.getTheaterMovies(this.butlerID).subscribe(
+      (response) => this.butlerMovies = response
+    ) ;
+    this.moviesService.getAllMovies().subscribe(
+      (response) => this.allMovies = response
+    ) ;
+
   }
 
   doShowMovies() {
+    //loop through the movies
+    for(var movie of this.allMovies.movies){
+      //fetch showtimes 
+      this.moviesService.getShowtimesFromLocation(movie.id).subscribe(
+        (response) => this.allMovieTimes.push(response)
+      );
+    }
     this.showOptions = 'none';
     this.showTheaters = 'none';
     this.showMovies = 'block';
@@ -76,17 +90,40 @@ export class MoviesComponent implements OnInit {
     this.showOptions = 'block';
   }
 
+  doShowNewberry(){
+    //loop through the movies
+    for(var movie of this.newberryMovies.movies){
+      //fetch showtimes 
+      this.moviesService.getShowtimesFromTheater(this.newberryID, movie.id).subscribe(
+        (response) => this.newberryMovieTimes.push(response)
+      );
+    }
+    this.showTheaters='none';
+    this.showNewberry = 'block';
+  }
+  doShowCelebration(){
+    //loop through the movies
+    for(var movie of this.celebrationMovies.movies){
+      //fetch showtimes 
+      this.moviesService.getShowtimesFromTheater(this.celebrationID, movie.id).subscribe(
+        (response) => this.celebrationMovieTimes.push(response)
+      );
+    }
+    this.showTheaters='none';
+    this.showCelebration = 'block';
+  }
+
   doShowButler(){
+    //loop through the movies
+    for(var movie of this.butlerMovies.movies){
+      //fetch showtimes 
+      this.moviesService.getShowtimesFromTheater(this.butlerID, movie.id).subscribe(
+        (response) => this.butlerMovieTimes.push(response)
+      );
+    }
     this.showTheaters='none';
     this.showButler = 'block';
-    console.log(this.moviesService.butlerMovies);
-    // for(var movie of this.butlerMovies.movies){
-    //   console.log('in the loop!');
-    //   //fetch showtime
-    //   var newTimes;
-    //   this.moviesService.getShowtimesFromTheater(movie.id);
-    //   this.butlerMovieTimes.push(newTimes);
-    // }
   }
+
 
 }
